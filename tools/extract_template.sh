fd8abc94be33a50f25d82c28d2fbdabab58acb50
Cédric Anne <cedric.anne@gmail.com>
2018-06-21 12:58:21 +0200

    Extract en_GB verbatim from pot file

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -51,8 +51,11 @@ if [ ! -d "locales" ]; then
 fi
 
 # Only strings with domain specified are extracted (use Xt args of keyword param to set number of args needed)
-xgettext $PHP_SOURCES -o locales/$POTFILE -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
-      --keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t --keyword=_ex:1c,2,3t \
-      --keyword=_sx:1c,2,3t --keyword=_nx:1c,2,3,5t
+xgettext $PHP_SOURCES -o locales/$POTFILE -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po \
+    --keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t --keyword=_ex:1c,2,3t \
+    --keyword=_sx:1c,2,3t --keyword=_nx:1c,2,3,5t
+
+#Update main language
+LANG=C msginit --no-translator -i locales/$POTFILE -l en_GB -o locales/en_GB.po
 
 popd > /dev/null
